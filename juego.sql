7465b7ef446e69426eafb3e9a027762e38865d41
JAIRO456 <jairoestebanviverosvasquezz@gmail.com>
2025-03-09 15:31:39 -0500

    Reponsive 2 y Database

--- juego.sql
+++ juego.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-03-2025 a las 04:16:03
+-- Tiempo de generación: 09-03-2025 a las 21:27:46
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `armas` (
   `ID_arma` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
   `danio` int(11) NOT NULL,
   `municion_max` int(11) DEFAULT NULL,
   `imagen_armas` varchar(500) NOT NULL,
@@ -40,24 +41,17 @@ CREATE TABLE `armas` (
 -- Volcado de datos para la tabla `armas`
 --
 
-INSERT INTO `armas` (`ID_arma`, `danio`, `municion_max`, `imagen_armas`, `ID_tipo`, `nivel_ar`) VALUES
-(1, 1, NULL, 'puño.jpg', 1, 1),
-(2, 5, 12, 'pistola.jpg', 2, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `estadisticas`
---
-
-CREATE TABLE `estadisticas` (
-  `ID_estadisticas` int(11) NOT NULL,
-  `partidas_ganadas` int(11) DEFAULT 0,
-  `partidas_perdidas` int(11) DEFAULT 0,
-  `eliminaciones_totales` int(11) DEFAULT 0,
-  `dano_total` int(11) DEFAULT 0,
-  `ID_usuario` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `armas` (`ID_arma`, `nombre`, `danio`, `municion_max`, `imagen_armas`, `ID_tipo`, `nivel_ar`) VALUES
+(1, 'Puño', 1, NULL, 'puño.jpg', 1, 1),
+(2, 'Pistola', 2, 12, 'pistola.jpg', 2, 1),
+(3, 'Ametralladora', 10, 30, 'ametralladora.jpg', 4, 2),
+(4, 'francotirador.jpg', 20, 4, 'francotirador.jpg', 3, 2),
+(5, 'Usp', 2, 10, 'usp.jpg', 2, 1),
+(6, 'Revolver', 2, 6, 'revolver.jpg', 2, 1),
+(7, 'Xm8', 11, 30, 'xm8.jpg', 4, 2),
+(8, 'Mp 40', 10, 30, 'mp40.jpg', 4, 2),
+(11, 'SVD', 20, 6, 'svd.jpg', 3, 2),
+(12, 'M82B', 20, 2, 'm82b.jpg', 3, 2);
 
 -- --------------------------------------------------------
 
@@ -68,6 +62,7 @@ CREATE TABLE `estadisticas` (
 CREATE TABLE `mapas` (
   `ID_mapas` int(11) NOT NULL,
   `mapas` varchar(100) NOT NULL,
+  `nivel_requerido` int(1) NOT NULL,
   `imagen_mapas` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -75,9 +70,9 @@ CREATE TABLE `mapas` (
 -- Volcado de datos para la tabla `mapas`
 --
 
-INSERT INTO `mapas` (`ID_mapas`, `mapas`, `imagen_mapas`) VALUES
-(1, 'BR-CLASIFICATORIA', 'BR-CLASIFICATORIA.png'),
-(2, 'DE-CLASIFICATORIA', 'DE-CLASIFICATORIA.png');
+INSERT INTO `mapas` (`ID_mapas`, `mapas`, `nivel_requerido`, `imagen_mapas`) VALUES
+(1, 'BR-CLASIFICATORIA', 1, 'BR-CLASIFICATORIA.png'),
+(2, 'DE-CLASIFICATORIA', 2, 'DE-CLASIFICATORIA.png');
 
 -- --------------------------------------------------------
 
@@ -90,21 +85,23 @@ CREATE TABLE `partidas` (
   `fecha_inicio` datetime NOT NULL,
   `fecha_fin` datetime NOT NULL,
   `ID_usuario` int(11) DEFAULT NULL,
-  `ID_sala` int(11) DEFAULT NULL
+  `ID_sala` int(11) DEFAULT NULL,
+  `puntos_partida` int(11) DEFAULT 0,
+  `dano_total` int(11) DEFAULT 0,
+  `headshots` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `partidas`
 --
 
-INSERT INTO `partidas` (`ID_partida`, `fecha_inicio`, `fecha_fin`, `ID_usuario`, `ID_sala`) VALUES
-(1, '2025-03-06 21:31:19', '2025-03-06 21:31:19', 9, 1),
-(2, '2025-03-06 21:31:19', '2025-03-06 21:31:19', 6, 1),
-(3, '2025-03-06 21:31:19', '2025-03-06 21:31:19', 4, 2),
-(4, '2025-03-06 21:31:19', '2025-03-06 21:31:19', 8, 2),
-(5, '2025-03-06 21:31:19', '2025-03-06 21:31:19', 5, 2),
-(6, '2025-03-06 21:31:19', '2025-03-06 21:31:19', 7, 2),
-(7, '2025-03-06 21:31:19', '2025-03-06 21:31:19', 4, 1);
+INSERT INTO `partidas` (`ID_partida`, `fecha_inicio`, `fecha_fin`, `ID_usuario`, `ID_sala`, `puntos_partida`, `dano_total`, `headshots`) VALUES
+(94, '2025-03-08 21:47:27', '0000-00-00 00:00:00', 4, 24, 3, 3, 0),
+(95, '2025-03-08 21:47:28', '0000-00-00 00:00:00', 9, 24, 22, 22, 0),
+(96, '2025-03-08 21:47:30', '0000-00-00 00:00:00', 9, 24, 22, 22, 0),
+(97, '2025-03-08 21:47:31', '0000-00-00 00:00:00', 4, 24, 3, 3, 0),
+(98, '2025-03-08 21:47:33', '0000-00-00 00:00:00', 9, 9, 22, 22, 0),
+(104, '2025-03-09 21:15:43', '0000-00-00 00:00:00', 1, 9, 108, 10, 0);
 
 -- --------------------------------------------------------
 
@@ -117,19 +114,29 @@ CREATE TABLE `salas` (
   `nombre_sala` varchar(100) NOT NULL,
   `jugadores` int(11) NOT NULL,
   `nivel_requerido` int(11) NOT NULL,
-  `ID_mapas` int(11) DEFAULT NULL
+  `ID_mapas` int(11) DEFAULT NULL,
+  `ID_estado` int(11) NOT NULL,
+  `tiempo_inicio` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `salas`
 --
 
-INSERT INTO `salas` (`ID_sala`, `nombre_sala`, `jugadores`, `nivel_requerido`, `ID_mapas`) VALUES
-(1, 'BR-CLASIFICATORIA', 5, 1, 1),
-(2, 'DE-CLASIFICATORIA', 5, 2, 2),
-(6, 'Sala 2', 5, 0, 2),
-(7, 'Sala 3', 3, 0, 2),
-(8, 'Sala 2', 0, 0, 1);
+INSERT INTO `salas` (`ID_sala`, `nombre_sala`, `jugadores`, `nivel_requerido`, `ID_mapas`, `ID_estado`, `tiempo_inicio`) VALUES
+(2, 'sala_prueba', 5, 0, 2, 0, '2025-03-08 00:00:56'),
+(3, 'PRUEBA_SALA_F', 5, 1, 1, 0, '2025-03-08 00:00:56'),
+(4, 'PRUEBA_SALA_F', 5, 1, 1, 0, '2025-03-08 00:00:56'),
+(9, 'PRUEBA_SALA_F', 5, 1, 1, 0, '2025-03-08 00:00:56'),
+(18, 'Sala 2', 5, 0, 2, 0, '2025-03-08 03:48:59'),
+(19, 'Sala 3', 5, 0, 2, 0, '2025-03-08 04:24:33'),
+(20, 'Sala 4', 5, 0, 2, 0, '2025-03-08 04:25:10'),
+(21, 'Sala 5', 5, 0, 2, 0, '2025-03-08 18:53:00'),
+(22, 'Sala 6', 5, 0, 2, 0, '2025-03-08 20:16:32'),
+(23, 'Sala 7', 5, 0, 2, 0, '2025-03-08 20:27:27'),
+(24, 'Sala 8', 5, 0, 2, 0, '2025-03-08 20:45:41'),
+(25, 'Sala 9', 0, 0, 2, 0, '2025-03-08 23:06:15'),
+(26, 'Sala 4', 0, 0, 1, 0, '2025-03-09 20:22:52');
 
 --
 -- Índices para tablas volcadas
@@ -141,13 +148,6 @@ INSERT INTO `salas` (`ID_sala`, `nombre_sala`, `jugadores`, `nivel_requerido`, `
 ALTER TABLE `armas`
   ADD PRIMARY KEY (`ID_arma`),
   ADD KEY `ID_tipo` (`ID_tipo`);
-
---
--- Indices de la tabla `estadisticas`
---
-ALTER TABLE `estadisticas`
-  ADD PRIMARY KEY (`ID_estadisticas`),
-  ADD KEY `ID_usuario` (`ID_usuario`);
 
 --
 -- Indices de la tabla `mapas`
@@ -175,22 +175,28 @@ ALTER TABLE `salas`
 --
 
 --
--- AUTO_INCREMENT de la tabla `estadisticas`
+-- AUTO_INCREMENT de la tabla `armas`
 --
-ALTER TABLE `estadisticas`
-  MODIFY `ID_estadisticas` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `armas`
+  MODIFY `ID_arma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `mapas`
+--
+ALTER TABLE `mapas`
+  MODIFY `ID_mapas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `partidas`
 --
 ALTER TABLE `partidas`
-  MODIFY `ID_partida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `ID_partida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
 
 --
 -- AUTO_INCREMENT de la tabla `salas`
 --
 ALTER TABLE `salas`
-  MODIFY `ID_sala` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID_sala` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Restricciones para tablas volcadas
@@ -201,12 +207,6 @@ ALTER TABLE `salas`
 --
 ALTER TABLE `armas`
   ADD CONSTRAINT `armas_ibfk_1` FOREIGN KEY (`ID_tipo`) REFERENCES `tipo` (`ID_tipo`);
-
---
--- Filtros para la tabla `estadisticas`
---
-ALTER TABLE `estadisticas`
-  ADD CONSTRAINT `estadisticas_ibfk_1` FOREIGN KEY (`ID_usuario`) REFERENCES `usuario` (`ID_usuario`);
 
 --
 -- Filtros para la tabla `partidas`
